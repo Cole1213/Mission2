584e91ff4f732d38f2ff4570b18fd213223590cd
Cole1213 <150396075+Cole1213@users.noreply.github.com>
2024-01-17 19:06:39 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using ConsoleApp1;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Import RandArray class
+        RandArray ar;
+        ar = new RandArray();
+        
+        //Gather user input
+        Console.WriteLine("Welcome to the dice throwing simulator!\n\nHow many rolls would you like to simulate?");
+        int numRolls = int.Parse(Console.ReadLine());
+
+        //Pass data to a new class "RandArray"
+        //Assign the returned array to 'result'
+        int[] result = ar.createArray(numRolls);
+
+        //Write the results
+        Console.WriteLine("\nDICE ROLLING SIMULATION RESULTS\n" +
+            "Each '*' represents 1% of the total number of rolls.\n" +
+            "Total number of rolls = " +  numRolls + ".\n");
+
+        for (int i = 2; i < result.Length; i++) //for every spot in the array
+        {
+            //calulate the percentage
+            int pct = result[i] * 100 / numRolls;
+
+            //Write the number we are looking at
+            Console.Write(i + ": ");
+            
+            //Write an asterisk for the percent value
+            for (int j = 0; j < pct; j++)
+            {
+                Console.Write("*");
+            }
+            Console.Write("\n");
+        }
+
+        //thank the user
+        Console.WriteLine("\nThank you for using the dice throwing simulator.  Goodbye!");
+
+    }
+}
---
+++ RandArray.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class RandArray
+    {
+        public int[] createArray(int num)
+        {
+            //Create the empty array
+            int[] rollTotals = new int[13];
+
+            //initialize variables
+            int die1 = 0;
+            int die2 = 0;
+            int total = 0;
+
+            //Initialize random object
+            Random rnd = new Random();
+
+            for (int i = 0; i < num; i++) //loop through num
+            {
+                die1 = rnd.Next(1, 7); //random number
+                die2 = rnd.Next(1, 7); //random number
+                total = die1 + die2;
+
+                rollTotals[total]++; //Increment array accordingly
+            }
+            
+            //Return rolls array
+            return rollTotals;
+            
+        }
+    }
+}
